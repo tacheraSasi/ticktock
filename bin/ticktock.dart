571932ea638e6e7e2a7eacc3f4854316d67bdc3f
tacherasasi <tacherasasi@gmail.com>
2025-05-17 22:59:42 +0300

    more changes

--- bin/ticktock.dart
+++ bin/ticktock.dart
@@ -1,10 +1,27 @@
 import 'dart:io';
-const String runCmd  = "dart run ticktock.dart";
+const runCmd  = "dart run ticktock.dart";
+const sessionFile = 'ticktock.json';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   // print(arguments);
   if(arguments.isEmpty){
     print("⏱️ Usage: $runCmd <start|stop|status|clear>");
     exit(0);
+  }
+
+  final command = arguments[0];
+
+  switch (command) {
+    case 'start':
+      await startSession();   
+      break;
+    case 'stop':
+      await stopSession();
+      break;
+    case 'status':
+      await showStatus();
+      break;
+    default:
+      print("❌ Unknown command: $command");
   }
 }
